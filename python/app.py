22f1eccac50623decb722fddc6bb11a106091ebd
Yok4ai <yokai.re77@gmail.com>
2025-04-23 11:03:19 +0600

    N/A

--- python/app.py
+++ python/app.py
@@ -666,4 +666,5 @@ def home():
 
 if __name__ == '__main__':
     # Get port from environment (for Render compatibility)
-    port = int(os.environ.get('PORT',
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
